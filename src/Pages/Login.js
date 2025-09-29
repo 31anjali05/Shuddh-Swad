ce552a04a7feb028fad1fe577e46294a2cc60ad8
Mona Lisa <amanmishra1321@gmail.com>
2025-09-29 22:54:54 +0530

    Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,82 +1,62 @@
-// import React from "react";
-// import logo from "./logo.png";
-// import { RiMenuLine } from "react-icons/ri";
-// import { IoSearchOutline } from "react-icons/io5";
-// import { VscAccount } from "react-icons/vsc";
-
-// function Login() {
-//     return (
-//          <>
-//
-//           <Router>
-//           <header>
-//             <a
-//               className="btn"
-//               data-bs-toggle="offcanvas"
-//               href="#offcanvasExample"
-//               role="button"
-//               aria-controls="offcanvasExample"
-//               style={{ textAlign: "left !important" }}
-//             >
-//               <RiMenuLine
-//                 style={{
-//                   height: "20px",
-//                   width: "20px",
-//                   // backgroundColor: "#f2efdd",
-//                   // color: "black",
-//                   // bsBtnBg: "#f2efdd",
-//                   // border: "none",
-//                 }}
-//               />
-//             </a>
-
-//             <IoSearchOutline
-//               style={{
-//                 height: "20px",
-//                 width: "20px",
-//                 textAlign: "left !important",
-//                 marginLeft: "5px",
-//               }}
-//             />
-//             <img src={logo} alt="Logo" className="Logo" />
-//             <VscAccount
-//               style={{
-//                 height: "20px",
-//                 width: "20px",
-//               }}
-//             />
-//           </header>
-//       </Router>
-// </>
-//     );
-// }
-// export default Log;
-
-import React from "react";
-// import logo from "./logo.png";
-import { RiMenuLine } from "react-icons/ri";
-import { IoSearchOutline } from "react-icons/io5";
-import { VscAccount } from "react-icons/vsc";
+import "./Login.css";
+import React, { useState } from "react";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Login attempted with:", { email, password });
+  };
   return (
     <>
       <div style={{ backgroundColor: "#f2efdd" }}>
-        {/* <header style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <a
-            className="btn"
-            data-bs-toggle="offcanvas"
-            href="#offcanvasExample"
-            role="button"
-            aria-controls="offcanvasExample"
-          >
-            <RiMenuLine style={{ height: "20px", width: "20px" }} />
-          </a>
-
-          <IoSearchOutline style={{ height: "20px", width: "20px" }} />
-          <img src={logo} alt="Logo" className="Logo" />
-          <VscAccount style={{ height: "20px", width: "20px" }} />
-        </header> */}
+        <div
+          className="container "
+          style={{
+            textAlign: "center",
+            color: "#3d081b",
+            height: "710px",
+          }}
+        >
+          <p style={{ fontSize: "45px" }}>Login</p>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <FloatingLabel controlId="floatingInput" label="Email">
+                <Form.Control
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  required
+                  className="field"
+                />
+              </FloatingLabel>
+            </div>
+            <div>
+              <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  required
+                  className="field"
+                />
+              </FloatingLabel>
+            </div>
+            <route>
+              <link></link>
+            </route>
+            <button className="signInBtn" type="submit">
+              Sign in
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
